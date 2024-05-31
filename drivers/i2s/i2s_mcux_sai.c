d5c608639911a05f780a942fd3288d5ee53ccad8
Qiang Zhang <qiang.zhang_6@nxp.com>
2024-05-31 11:06:33 +0800

    drivers: drivers/i2s: fix sai issue for support frdm_mcxn497
      i2s driver have not suooprt frdm_mcxn947 pll clk set.
      so add macro CONFIG_I2S_HAS_PLL_SETTING to control pll init.
    
    Signed-off-by: Qiang Zhang <qiang.zhang_6@nxp.com>

--- drivers/i2s/i2s_mcux_sai.c
+++ drivers/i2s/i2s_mcux_sai.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021,2023 NXP Semiconductor INC.
+ * Copyright 2021,2023-2024 NXP Semiconductor INC.
  * All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -403,20 +403,6 @@ static void i2s_dma_rx_callback(const struct device *dma_dev, void *arg, uint32_
 	}
 }
 
-static void enable_mclk_direction(const struct device *dev, bool dir)
-{
-	const struct i2s_mcux_config *dev_cfg = dev->config;
-	uint32_t offset = dev_cfg->mclk_pin_offset;
-	uint32_t mask = dev_cfg->mclk_pin_mask;
-	uint32_t *gpr = (uint32_t *)(DT_REG_ADDR(DT_NODELABEL(iomuxcgpr)) + offset);
-
-	if (dir) {
-		*gpr |= mask;
-	} else {
-		*gpr &= ~mask;
-	}
-}
-
 static void get_mclk_rate(const struct device *dev, uint32_t *mclk)
 {
 	const struct i2s_mcux_config *dev_cfg = dev->config;
@@ -432,6 +418,34 @@ static void get_mclk_rate(const struct device *dev, uint32_t *mclk)
 		return;
 	}
 	*mclk = rate;
+}
+
+static void enable_mclk_direction(const struct device *dev, bool dir)
+{
+	const struct i2s_mcux_config *dev_cfg = dev->config;
+
+#ifdef CONFIG_I2S_HAS_PLL_SETTING
+	uint32_t offset = dev_cfg->mclk_pin_offset;
+	uint32_t mask = dev_cfg->mclk_pin_mask;
+	uint32_t *gpr = (uint32_t *)(DT_REG_ADDR(DT_NODELABEL(iomuxcgpr)) + offset);
+
+	if (dir) {
+		*gpr |= mask;
+	} else {
+		*gpr &= ~mask;
+	}
+#else
+	sai_master_clock_t mclkConfig;
+	uint32_t mclk;
+	I2S_Type *base = (I2S_Type *)dev_cfg->base;
+
+	get_mclk_rate(dev, &mclk);
+	mclkConfig.mclkOutputEnable = true;
+	mclkConfig.mclkHz = mclk;
+	mclkConfig.mclkSourceClkHz = mclk;
+	SAI_SetMasterClockConfig(base, &mclkConfig);
+#endif
+
 }
 
 static int i2s_mcux_config(const struct device *dev, enum i2s_dir dir,
@@ -1066,6 +1080,7 @@ static void i2s_mcux_isr(void *arg)
 
 static void audio_clock_settings(const struct device *dev)
 {
+#ifdef CONFIG_I2S_HAS_PLL_SETTING
 	clock_audio_pll_config_t audioPllConfig;
 	const struct i2s_mcux_config *dev_cfg = dev->config;
 	uint32_t clock_name = (uint32_t)dev_cfg->clk_sub_sys;
@@ -1091,6 +1106,7 @@ static void audio_clock_settings(const struct device *dev)
 #endif /* CONFIG_SOC_SERIES */
 
 	CLOCK_InitAudioPll(&audioPllConfig);
+#endif
 }
 
 static int i2s_mcux_initialize(const struct device *dev)
@@ -1149,7 +1165,8 @@ static int i2s_mcux_initialize(const struct device *dev)
 /* master clock configurations */
 #if (defined(FSL_FEATURE_SAI_HAS_MCR) && (FSL_FEATURE_SAI_HAS_MCR)) ||                             \
 	(defined(FSL_FEATURE_SAI_HAS_MCLKDIV_REGISTER) && (FSL_FEATURE_SAI_HAS_MCLKDIV_REGISTER))
-#if defined(FSL_FEATURE_SAI_HAS_MCLKDIV_REGISTER) && (FSL_FEATURE_SAI_HAS_MCLKDIV_REGISTER)
+#if ((defined(FSL_FEATURE_SAI_HAS_MCLKDIV_REGISTER) && (FSL_FEATURE_SAI_HAS_MCLKDIV_REGISTER)) || \
+	(defined(FSL_FEATURE_SAI_HAS_MCR_MCLK_POST_DIV) && (FSL_FEATURE_SAI_HAS_MCR_MCLK_POST_DIV)))
 	mclkConfig.mclkHz = mclk;
 	mclkConfig.mclkSourceClkHz = mclk;
 #endif
@@ -1176,17 +1193,17 @@ static const struct i2s_driver_api i2s_mcux_driver_api = {
                                                                                                    \
 	static const struct i2s_mcux_config i2s_##i2s_id##_config = {                              \
 		.base = (I2S_Type *)DT_INST_REG_ADDR(i2s_id),                                      \
-		.clk_src = DT_INST_PROP(i2s_id, clock_mux),                                        \
-		.clk_pre_div = DT_INST_PROP(i2s_id, pre_div),                                      \
-		.clk_src_div = DT_INST_PROP(i2s_id, podf),                                         \
-		.pll_src = DT_PHA_BY_NAME(DT_DRV_INST(i2s_id), pll_clocks, src, value),            \
-		.pll_lp = DT_PHA_BY_NAME(DT_DRV_INST(i2s_id), pll_clocks, lp, value),              \
-		.pll_pd = DT_PHA_BY_NAME(DT_DRV_INST(i2s_id), pll_clocks, pd, value),              \
-		.pll_num = DT_PHA_BY_NAME(DT_DRV_INST(i2s_id), pll_clocks, num, value),            \
-		.pll_den = DT_PHA_BY_NAME(DT_DRV_INST(i2s_id), pll_clocks, den, value),            \
-		.mclk_pin_mask = DT_PHA_BY_IDX(DT_DRV_INST(i2s_id), pinmuxes, 0, function),        \
-		.mclk_pin_offset = DT_PHA_BY_IDX(DT_DRV_INST(i2s_id), pinmuxes, 0, pin),           \
-		.clk_sub_sys =                                                                     \
+		.clk_src = DT_INST_PROP_OR(i2s_id, clock_mux, 0),   \
+		.clk_pre_div = DT_INST_PROP_OR(i2s_id, pre_div, 0), \
+		.clk_src_div = DT_INST_PROP_OR(i2s_id, podf, 0),    \
+		.pll_src = DT_PHA_BY_NAME_OR(DT_DRV_INST(i2s_id), pll_clocks, src, value, 0),     \
+		.pll_lp = DT_PHA_BY_NAME_OR(DT_DRV_INST(i2s_id), pll_clocks, lp, value, 0),       \
+		.pll_pd = DT_PHA_BY_NAME_OR(DT_DRV_INST(i2s_id), pll_clocks, pd, value, 0),       \
+		.pll_num = DT_PHA_BY_NAME_OR(DT_DRV_INST(i2s_id), pll_clocks, num, value, 0),     \
+		.pll_den = DT_PHA_BY_NAME_OR(DT_DRV_INST(i2s_id), pll_clocks, den, value, 0),     \
+		.mclk_pin_mask = DT_PHA_BY_IDX_OR(DT_DRV_INST(i2s_id), pinmuxes, 0, function, 0), \
+		.mclk_pin_offset = DT_PHA_BY_IDX_OR(DT_DRV_INST(i2s_id), pinmuxes, 0, pin, 0),    \
+		.clk_sub_sys =                                                                    \
 			(clock_control_subsys_t)DT_INST_CLOCKS_CELL_BY_IDX(i2s_id, 0, name),       \
 		.ccm_dev = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(i2s_id)),                             \
 		.irq_connect = i2s_irq_connect_##i2s_id,                                           \
