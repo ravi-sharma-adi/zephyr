5235b46697a07431902ce4333a429d426b85db26
Duy Nguyen <duy.nguyen.xa@renesas.com>
2024-08-19 12:28:00 +0700

    soc: renesas: rx: Initial support for RX130 SOC
    Minimal SOC layer support for Renesas RX SOC
    This SOC is using Renesas RXv1 core
    
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

---
+++ include/zephyr/dt-bindings/pinctrl/renesas/pinctrl-rx.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2024 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_PINCTRL_RX_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_PINCTRL_RX_H_
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_SOC_SAM_COMMON_H_ */
