32d89733707cfc533e06176c8a2421358291e94d
Iuliana Prodan <iuliana.prodan@nxp.com>
2024-10-16 17:20:21 +0300

    boards: nxp: imxrt700: Add i.MXRT700 HiFi4 board support
    Add board support for the HiFi4 DSP core from NXP i.MXRT700.
    
    Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

---
+++ boards/nxp/mimxrt700_evk/board.cmake
@@ -0,0 +1,8 @@
+#
+# Copyright 2024 NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
