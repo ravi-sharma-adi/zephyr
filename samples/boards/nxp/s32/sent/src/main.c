anilmaguluri/zephyr_ble

samples: boards: nxp: add sample for using SENT driver
Add sample for using SENT driver on s32z

Signed-off-by: Cong Nguyen Huu <cong.nguyenhuu@nxp.com>

samples/boards/nxp/s32/sent/src/main.c
@@ -1,0 +1,31 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(nxp_s32_sent_sample, LOG_LEVEL_DBG);
+
+#include <zephyr/kernel.h>
+
+#include <zephyr/drivers/sent/sent.h>
+
+#define SENT_NODE DT_INST(0, nxp_s32_sent_controller)
+
+void rx_cb(const struct device *dev, uint8_t channel_id, struct sent_frame *frame,
+	   enum sent_state state, void *user_data)
+{
+	LOG_INF("rx ch %d", channel_id);
+}
+
+int main(void)
+{
+	const struct device *const dev = DEVICE_DT_GET(SENT_NODE);
+
+	sent_add_rx_callback(dev, 1, rx_cb, NULL);
+
+	sent_start(dev, 1);
+
+	return 0;
+}

ravi-sharma-adi/zephyr

samples/boards/nxp/s32/sent/src/main.c
@@ -1,0 +1,31 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(nxp_s32_sent_sample, LOG_LEVEL_DBG);
+
+#include <zephyr/kernel.h>
+
+#include <zephyr/drivers/sent/sent.h>
+
+#define SENT_NODE DT_INST(0, nxp_s32_sent)
+
+void rx_cb(const struct device *dev, uint8_t channel_id, struct sent_frame *frame,
+	   enum sent_state state, void *user_data)
+{
+	LOG_INF("rx ch %d", channel_id);
+}
+
+int main(void)
+{
+	const struct device *const dev = DEVICE_DT_GET(SENT_NODE);
+
+	sent_add_rx_callback(dev, 1, rx_cb, NULL);
+
+	sent_start(dev, 1);
+
+	return 0;
+}