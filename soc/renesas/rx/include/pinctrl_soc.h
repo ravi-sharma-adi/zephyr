b7afa37312e125549db976224e5ea8c7e4c168e3
Duy Nguyen <duy.nguyen.xa@renesas.com>
2024-11-04 15:22:45 +0700

    drivers: pinctrl: Support pinctrl driver for Renesas RX
    Intial support of pinctrl driver for Renesas RX MCU
    family.
    This support base on using Renesas RX driver package in
    hal_renesas layer
    
    Signed-off-by: Phi Tran <phi.tran.jg@bp.renesas.com>
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

--- soc/renesas/rx/include/pinctrl_soc.h
+++ soc/renesas/rx/include/pinctrl_soc.h
@@ -4,27 +4,44 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef ZEPHYR_SOC_RENESAS_RA_COMMON_PINCTRL_SOC_H_
-#define ZEPHYR_SOC_RENESAS_RA_COMMON_PINCTRL_SOC_H_
+#ifndef ZEPHYR_SOC_RENESAS_RX_COMMON_PINCTRL_SOC_H_
+#define ZEPHYR_SOC_RENESAS_RX_COMMON_PINCTRL_SOC_H_
 
+#include <stdbool.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/types.h>
 
 #include <zephyr/dt-bindings/pinctrl/renesas/pinctrl-rx.h>
 
+#define PORT_POS (8)
+
+#define PIN_MODE_GPIO       (0)
+#define PIN_MODE_PERIPHERAL (1)
+
+struct rx_pin_config {
+	bool pin_mode;
+	bool analog_enable;
+	bool output_enable;
+	bool output_high;
+	bool bias_pull_up;
+	bool drive_open_drain;
+	uint8_t drive_strength;
+	uint8_t psels;
+};
+
 /**
  * @brief Type to hold a renesas ra pin's pinctrl configuration.
  */
-struct ra_pinctrl_soc_pin {
-	/** Port number 0..9, A, B */
-	uint32_t port_num: 4;
-	/** Pin number 0..15 */
+struct rx_pinctrl_soc_pin {
+	/** Port number 0..5, A..E, H, J */
+	uint32_t port_num: 5;
+	/** Pin number 0..7 */
 	uint32_t pin_num: 4;
-	/** Register PFS cfg */
-	uint32_t cfg;
+	/** config pin */
+	struct rx_pin_config cfg;
 };
 
-typedef struct ra_pinctrl_soc_pin pinctrl_soc_pin_t;
+typedef struct rx_pinctrl_soc_pin pinctrl_soc_pin_t;
 
 /**
  * @brief Utility macro to initialize each pin.
@@ -35,13 +52,19 @@ typedef struct ra_pinctrl_soc_pin pinctrl_soc_pin_t;
  */
 #define Z_PINCTRL_STATE_PIN_INIT(node_id, prop, idx)                                               \
 	{                                                                                          \
-		.port_num = RA_GET_PORT_NUM(DT_PROP_BY_IDX(node_id, prop, idx)),                   \
-		.pin_num = RA_GET_PIN_NUM(DT_PROP_BY_IDX(node_id, prop, idx)),                     \
-		.cfg = (DT_PROP(node_id, bias_pull_up) << 4) |                                     \
-		       (DT_PROP(node_id, drive_open_drain) << 6) |                                 \
-		       (DT_ENUM_IDX(node_id, drive_strength) << 10) |                              \
-		       (RA_GET_MODE(DT_PROP_BY_IDX(node_id, prop, idx)) << 16) |                   \
-		       (RA_GET_PSEL(DT_PROP_BY_IDX(node_id, prop, idx)) << 24),                    \
+		.port_num = RX_GET_PORT_NUM(DT_PROP_BY_IDX(node_id, prop, idx)),                   \
+		.pin_num = RX_GET_PIN_NUM(DT_PROP_BY_IDX(node_id, prop, idx)),                     \
+		.cfg =                                                                             \
+			{                                                                          \
+				.pin_mode = PIN_MODE_PERIPHERAL,                                   \
+				.analog_enable = DT_PROP(node_id, renesas_analog_enable),          \
+				.output_enable = DT_PROP(node_id, output_enable),                  \
+				.output_high = DT_PROP(node_id, output_high),                      \
+				.bias_pull_up = DT_PROP(node_id, bias_pull_up),                    \
+				.drive_open_drain = DT_PROP(node_id, drive_open_drain),            \
+				.drive_strength = DT_ENUM_IDX(node_id, drive_strength),            \
+				.psels = RX_GET_PSEL(DT_PROP_BY_IDX(node_id, prop, idx)),          \
+			},                                                                         \
 	},
 
 /**
@@ -54,10 +77,9 @@ typedef struct ra_pinctrl_soc_pin pinctrl_soc_pin_t;
 	{DT_FOREACH_CHILD_VARGS(DT_PHANDLE(node_id, prop), DT_FOREACH_PROP_ELEM, psels,            \
 				Z_PINCTRL_STATE_PIN_INIT)}
 
-#define RA_GET_PORT_NUM(pinctrl) (((pinctrl) >> RA_PORT_NUM_POS) & RA_PORT_NUM_MASK)
-#define RA_GET_PIN_NUM(pinctrl)  (((pinctrl) >> RA_PIN_NUM_POS) & RA_PIN_NUM_MASK)
+#define RX_GET_PORT_NUM(pinctrl) (((pinctrl) >> RX_PORT_NUM_POS) & RX_PORT_NUM_MASK)
+#define RX_GET_PIN_NUM(pinctrl)  (((pinctrl) >> RX_PIN_NUM_POS) & RX_PIN_NUM_MASK)
 
-#define RA_GET_MODE(pinctrl) (((pinctrl) >> RA_MODE_POS) & RA_MODE_MASK)
-#define RA_GET_PSEL(pinctrl) (((pinctrl) >> RA_PSEL_POS) & RA_PSEL_MASK)
+#define RX_GET_PSEL(pinctrl) (((pinctrl) >> RX_PSEL_POS) & RX_PSEL_MASK)
 
-#endif /* ZEPHYR_SOC_RENESAS_RA_COMMON_PINCTRL_SOC_H_ */
+#endif /* ZEPHYR_SOC_RENESAS_RX_COMMON_PINCTRL_SOC_H_ */
